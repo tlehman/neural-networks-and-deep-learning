fdb2ca3b417fc25aca9b1896a8b17f7ff501ba7a
Tobi Lehman <tobi.lehman@gmail.com>
2015-11-15 13:55:05 -0800

    Add web wrapper for network interaction

---
+++ src/recognize.py
@@ -0,0 +1,30 @@
+# web wrapper for MNIST neural network recognizer
+import web
+import json
+
+# from Michael Nielson's book on neural networks and deep learning
+from network import *
+
+urls = (
+    '/(.*)', 'recognize'
+)
+app = web.application(urls, globals())
+
+# Load MNIST data
+import mnist_loader
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+# Initialize neural network
+net = Network([784, 100, 10])
+
+# Load trained weights and values (30 epochs, minibatch size of 10, eta=3.0)
+# net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
+
+class recognize:
+    def POST(self, name):
+        data = json.loads(web.data())
+        return "This is your img data: %s" % data['img']
+
+if __name__ == "__main__":
+    app.run()
